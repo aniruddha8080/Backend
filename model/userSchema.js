8cb737cb3451a8238b2f9368d468e1cde3483b94
aniruddha <aniruddhabhandare292@gmail.com>
2025-01-02 13:49:59 +0530

    first code

--- model/userSchema.js
+++ model/userSchema.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { type } = require('os');
 mongoose.connect("mongodb://localhost:27017")
 const userSchema = new mongoose.Schema({
-    name:String,
-    email: String,
-    password: Number
-});
 
+  name:{
+type:String,
+required:true
+  },
+  email:{
+type:String,
+required:true
+}
+  password :{
+    type:String,
+    required:true
+}
+);
 module.exports = mongoose.model('User', userSchema);
-
-
-    
--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-
-
 const express = require ("express");
 const app  = express();
-const port = 3000;
+const port = 4000;
 const { default: mongoose } = require('mongoose');
-
+const userSchema = require('./model/userSchema.js');
 const connectDB = require("./config/db.js")
-mongoose.connect("mongodb://localhost:27017/MERN")
+// mongoose.connect("mongodb://localhost:27017/MERN")
+console.log("connect mongodb")
 
-
-
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+ 
+app.post("/anni", async (req,res) => {
+  try{
+    const {email,password,name} = req.body;
+    const userExist =  userModel.findOne({email})
+    if(userExist){
 
-
-
+      return res.send({message: "User already exist"})
+    }
+    const user = await userModel.create(req.body.name)
+    UserModel.save();
+    return res.send({message: "User created successfully"})
+}
+catch (err) {
+  return res.send (error)
+}
+});
 app.listen(port, () => {
   console.log( "Server is running on" )
 });
